0a8af4b631ab689b143292fe3bf9f9f6b6ed9c4b
Ruslan <nikolrusik@gmail.com>
2023-03-01 01:58:55 +0300

    Added url, view, template and migrations. Registered Menu model on admin-panel

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainapp.views import MenuView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('menu/', MenuView.as_view(), name="menu_view")
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Menu
 
-# Create your views here.
+class MenuView(View):
+    def get(self, request):
+        context = {
+            # 'main_menu': Menu.objects.filter(name='main_menu').first(),
+            # 'footer_menu': Menu.objects.filter(name='footer_menu').first(),
+        }
+        return render(request, 'mainapp/menu.html', context)
